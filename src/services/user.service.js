93d98122f48d5a8f5d728436a60aec593185a26d
umer mushtaq <umermushtaq3424@gmail.com>
2022-05-17 11:50:05 +0500

    userName function name fix

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -10,7 +10,7 @@ const ApiError = require('../utils/ApiError');
 const createUser = async (userBody) => {
   if (await User.isEmailTaken(userBody.email)) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
-  } else if (await User.isUsernameTaken(userBody.username)) {
+  } else if (await User.isUserNameTaken(userBody.username)) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Username already taken');
   }
   return User.create(userBody);
